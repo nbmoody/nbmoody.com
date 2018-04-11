09849891c40f2d0bb198df1d7e1419bc65b41561
Nathaniel Moody <nathanielmoody@live.com>
2018-04-10 21:07:13 -0400

    Setup temp project-data schema in controller, adjust view to use data.

--- app_server/controllers/controllers.js
+++ app_server/controllers/controllers.js
@@ -6,7 +6,45 @@ const welcome = function(req, res) {
 
 /* GET 'portfolio' page */
 const portfolio = function(req, res) {
-  res.render('portfolio', {title:'Portfolio'});
+  res.render('portfolio', {
+    projects: [
+      {
+        imagePath: "/images/placeholder.png",
+        title: "Project Title",
+        linkURL: "#",
+        tags: ["Plotly", "Github", "Data Science", "Shiny"],
+        summary: "This is a really basic summary of this project. It is so fascinating. Brace yo-self..."
+      },
+      {
+        imagePath: "/images/placeholder.png",
+        title: "Project Title",
+        linkURL: "#",
+        tags: ["Plotly", "Github", "Data Science", "Shiny"],
+        summary: "This is a really basic summary of this project. It is so fascinating. Brace yo-self..."
+      },
+      {
+        imagePath: "/images/placeholder.png",
+        title: "Project Title",
+        linkURL: "#",
+        tags: ["Plotly", "Github", "Data Science", "Shiny"],
+        summary: "This is a really basic summary of this project. It is so fascinating. Brace yo-self..."
+      },
+      {
+        imagePath: "/images/placeholder.png",
+        title: "Project Title",
+        linkURL: "#",
+        tags: ["Plotly", "Github", "Data Science", "Shiny"],
+        summary: "This is a really basic summary of this project. It is so fascinating. Brace yo-self..."
+      },
+      {
+        imagePath: "/images/placeholder.png",
+        title: "Project Title",
+        linkURL: "#",
+        tags: ["Plotly", "Github", "Data Science", "Shiny"],
+        summary: "This is a really basic summary of this project. It is so fascinating. Brace yo-self..."
+      }
+    ]
+  });
 };
 
 /* GET 'about' page */
